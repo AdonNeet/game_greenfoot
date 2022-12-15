562a0bf0e61dca7fc7796b234f12c9750d5940fd
Mrzoee <99813006+Mrzoee@users.noreply.github.com>
2022-12-15 14:25:27 +0700

    create asset FlappyBird
    Semua aset dan tempat nyoret di branch ini

--- MyWorld.java
+++ MyWorld.java
@@ -16,6 +16,20 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(300,500 ,1); 
+        prepare();
+    }
+    
+    /**
+     * Prepare create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        start start = new start();
+        addObject(start,149,261);
+        bird bird = new bird();
+       addObject(bird,148,309);
+        base base = new base();
+        addObject(base,144,495);
     }
 }
